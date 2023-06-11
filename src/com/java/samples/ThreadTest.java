e7b1559d0a4db46c99f93ed8e06764da5306cce6
sureshtamilselvan <msgtosuresh@gmail.com>
2023-06-11 19:27:23 +0530

    first commit

---
+++ src/com/java/samples/ThreadTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.java.samples;
+
+/**
+ * @author Madhu Latha
+ *
+ */
+public class ThreadTest {
+
+	public void initiate(int i) {
+		System.out.println(i);
+	}
+}
---
+++ src/com/java/samples/dateTimeApi/ChronoEnums.java
@@ -0,0 +1,48 @@
+package com.java.samples.dateTimeApi;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoEnums {
+	 // Function to check ChronoUnit
+    public static void checkingChronoEnum()
+    {
+        LocalDate date = LocalDate.now();
+        System.out.println("current date is :" +
+                            date);
+     
+        // adding 2 years to the current date
+        LocalDate year =
+             date.plus(2, ChronoUnit.YEARS);
+         
+        System.out.println("next to next year is " +
+                            year);
+             
+        // adding 1 month to the current date
+        LocalDate nextMonth =
+                  date.plus(1, ChronoUnit.MONTHS);
+         
+        System.out.println("the next month is " +
+                            nextMonth);
+     
+        // adding 1 week to the current date
+        LocalDate nextWeek =
+                  date.plus(1, ChronoUnit.WEEKS);
+         
+        System.out.println("next week is " + nextWeek);
+     
+        // adding 2 decades to the current date
+        LocalDate Decade =
+                  date.plus(2, ChronoUnit.DECADES);
+                   
+        System.out.println("20 years after today " +
+                            Decade);
+    }
+     
+    // Driver code
+    public static void main(String[] args) {
+     
+        checkingChronoEnum();
+         
+    }
+}
